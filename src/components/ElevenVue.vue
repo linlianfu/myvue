1cf278e3c7999e57cbf94c6f11bee92fb9dec68e
linlianfu <1241352445@qq.com>
2019-10-22 22:19:22 +0800

    '初始化项目提交'

---
+++ src/components/ElevenVue.vue
@@ -0,0 +1,102 @@
+<template>
+
+  <div style="height: inherit">
+    <div style="height: 20px; width:100%;background-color: aquamarine">
+      <a :href="'http://localhost:9000'" style="float: left">返回</a>
+    </div>
+
+    <div style="width: 15%;min-height: 850px;background-color: coral;float: left;margin-left: -34px">
+      <div>
+        <ul>
+          <li v-for="menu in menuList" style="width: 100%">
+            {{menu.name}}
+          </li>
+        </ul>
+      </div>
+      <div>
+        <button @click = "action++">变换颜色</button>
+      </div>
+    </div>
+    <div style="width: 85%;height: 850px;background-color: cadetblue;float: right" v-if="action %2 === 0">
+
+       <p> 内容部分1</p>
+      <div>
+        测试使用v-text：<label v-text="count"></label>
+      </div>
+      <br>
+      计数：<label>{{count}}</label>
+      <br>
+      <button @click="count++">加1</button>
+      <button @click="count--">减1</button>
+    </div>
+    <div style="width: 85%;height: 850px;background-color: chartreuse;float: right" v-if="action %2 === 1">
+      <p>内容部分2</p>
+    </div>
+    <!--<div style="text-align: center">-->
+      <!--<h1>欢迎来到ElevenVue</h1>-->
+    <!--</div>-->
+    <!--<div>-->
+      <!--<button v-on:click="outputMessage()">打印消息</button>-->
+    <!--</div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ElevenVue',
+  data () {
+    return {
+      action:1,
+      count:0,
+      menuList:[
+        {
+          order:1,
+          name:"首页",
+          subMenu:[
+            {
+
+            }
+          ]
+        },
+        {
+          order:1,
+          name:"资讯管理",
+          subMenu:[
+            {
+              order:1,
+              name:"资讯分类",
+            },
+            {
+              order:2,
+              name:"资讯内容",
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods:{
+    outputMessage:function () {
+      console.log("2222222222222");
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="hello" style="text-align: center">
+    <a :href="'http://localhost:9000/#/eleven'"
+    style="float: right"> go ElevenVue</a>
+    <br>
+    <br>
+    <img src="../assets/logo.png" style="clear: both">
+
+
+
+    <h1>
+      请输入msg：<input v-model="msg"/><br/>
+      <p>msg:<label>{{msg}}</label></p>
+    </h1>
+    <div style="margin-top: 20px">
+      computedReversedMsg:<label>{{computedReversedMsg}}</label> ,date:<label>{{computedNow}}</label><br>
+      methodsReversedMsg:<label>{{methodsReversedMessage()}}</label>,date:<label>{{methodsNow()}}</label>
+    </div>
+    <div style="margin-top: 20px">
+      computedUpdateHelloWord:<label>{{computedUpdateHelloWord}}</label> <br>
+    </div>
+    <h2>Essential Links next is myself title</h2>
+    <br>
+    <a  :href="'http://'+address" target="_blank">百度</a>
+    <br>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank">Forum</a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank">Community Chat</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank">Twitter</a>
+      </li>
+      <br>
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'HelloWord',
+      title:"this is eleven title test v-bind",
+      address:"www.baidu.com",
+      helloWord:"HelloWord"
+    }
+  },
+  computed:{
+
+    //该方法默认只为该属性体统get方法
+    computedNow:function () {
+      return Date.now();
+    },
+    computedReversedMsg:function () {
+      return this.msg.split('').reverse().join('');
+    },
+    //同事提供get 个set属性的方法
+    computedUpdateHelloWord:{
+      get:function () {
+        // `this` 指向 vm 实例
+        this.computedUpdateHelloWord = "updateNewHelloWord";
+        return this.helloWord
+      },
+      set:function(newValue){
+        console.log("computedUpdateHelloWord setter被调用");
+        console.log(newValue);
+        this.helloWord = newValue;
+      }
+    }
+
+  },
+  methods:{
+    methodsReversedMessage:function () {
+      // `this` 指向 vm 实例
+      return this.msg.split('').reverse().join('')
+    },
+    methodsNow:function () {
+      // `this` 指向 vm 实例
+      return  Date.now();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
