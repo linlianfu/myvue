3285293fde25e522767ade78a2e94ea39758fad6
linlianfu <1241352445@qq.com>
2019-10-27 17:23:21 +0800

    项目标准化

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../components/home'
+import listOrder from '../views/listOrder'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: home
+    // },
     {
       path: '/',
       name: 'home',
-      component: home
-    },
-    // {
-    //   path: '/eleven',
-    //   name: 'ElevenVue',
-    //   component: elevenVue
-    // }
+      component: home,
+      children:[
+        {
+          name:"listOrder",
+          path:"/listOrder",
+          component:listOrder
+        }
+      ]
+    }
   ]
 })
