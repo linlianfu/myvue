e86483717aea39c914659ada338e21c283fd2701
linlianfu <1241352445@qq.com>
2019-10-27 17:38:34 +0800

    项目标准化

--- src/views/listOrder.vue
+++ src/views/listOrder.vue
@@ -24,21 +24,21 @@ export default {
   data () {
     return {
       tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
+        date: '2019-10-28',
+        name: 'eleven',
+        address: '福州市鼓楼区华博教育 1518 弄'
       }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
+        date: '2019-10-28',
+        name: 'eleven',
+        address: '福州市鼓楼区华博教育 1517 弄'
       }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
+        date: '2019-10-28',
+        name: 'eleven',
+        address: '福州市鼓楼区华博教育 1519 弄'
       }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
+        date: '2019-10-28',
+        name: 'eleven',
+        address: '福州市鼓楼区华博教育 1516 弄'
       }]
     }
   }
